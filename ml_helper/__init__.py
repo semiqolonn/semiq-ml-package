462207f181d8a2b13bec28cc8579ef04138e9122
Malfino Muhammad Willianz <willianz@pm.me>
2025-06-01 18:31:16 +0700

    will fix later

--- ml_helper/__init__.py
+++ ml_helper/__init__.py
@@ -9,10 +9,10 @@ __version__ = "0.1.0"
 
 # Import main classes for easy access
 from .baseline_model import BaselineModel
-from .hyperparameter_tuning import RandomSearchOptimizer
+# from .hyperparameter_tuning import RandomSearchOptimizer
 
 # Define what's available when using "from ml_helper import *"
 __all__ = [
     'BaselineModel',
-    'RandomSearchOptimizer',
+    # 'RandomSearchOptimizer',
 ]
