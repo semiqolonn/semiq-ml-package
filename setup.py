a126586f585855400e89ba6d3439bcd7971f4827
Malfino Muhammad Willianz <willianz@pm.me>
2025-06-02 19:43:16 +0700

    v0.1.0-release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="semiq-ml",
-    version="0.1",
+    version="0.1.0-release",
     packages=find_packages(include=["semiq_ml", "semiq_ml.*"]),
     install_requires=[
         # ... your dependencies ...
