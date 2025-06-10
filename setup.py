3423df21251b8ed11180b003653f1971fd897671
Malfino Muhammad Willianz <willianz@pm.me>
2025-06-11 02:15:23 +0700

    chore: Bump version to 0.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="semiq-ml",
-    version="0.3.2",
+    version="0.3.3",
     packages=find_packages(include=["semiq_ml", "semiq_ml.*"]),
     install_requires=[
         # ... your dependencies ...
