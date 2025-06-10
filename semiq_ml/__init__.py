c2cc44c6e13e453b39a845d577f8407e18f2b277
Malfino Muhammad Willianz <willianz@pm.me>
2025-06-11 02:03:48 +0700

    chore: Bump version to 0.3.0

--- semiq_ml/__init__.py
+++ semiq_ml/__init__.py
@@ -2,17 +2,19 @@
 Semiq-ML helper Package
 
 This package provides helper functions and classes to simplify common machine learning workflows,
-including baseline model training and hyperparameter tuning.
+including baseline model training, hyperparameter tuning, and image processing.
 """
 
-__version__ = "0.2.5"
+__version__ = "0.3.0"
 
 # Import main classes for easy access
 from .baseline_model import BaselineModel
 from .tuning import OptunaOptimizer
+import semiq_ml.image
 
 # Define what's available when using "from ml_helper import *"
 __all__ = [
     'BaselineModel',
     'OptunaOptimizer',
+    'image',
 ]
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="semiq-ml",
-    version="0.2.5",
+    version="0.3.0",
     packages=find_packages(include=["semiq_ml", "semiq_ml.*"]),
     install_requires=[
         # ... your dependencies ...
